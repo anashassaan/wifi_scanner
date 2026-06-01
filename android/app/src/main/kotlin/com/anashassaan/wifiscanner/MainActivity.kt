e613e116679ddda3a095c00297b4432805ebcfe3
Muhammad Anas Hassaan <manashassan6070@gmail.com>
2026-06-01 18:06:55 +0500

    done

--- android/app/src/main/kotlin/com/anashassaan/wifiscanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/anashassaan/wifiscanner/MainActivity.kt
@@ -2,9 +2,9 @@ package com.anashassaan.wifiscanner
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Enforce Android 15 Edge-to-Edge windowing guidelines
         enableEdgeToEdge()
